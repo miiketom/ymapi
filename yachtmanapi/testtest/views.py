68a512e0d8fbbeba8cfee056e477fbc1ef294383
miiketom <mtomczak14@edu.cdv.pl>
2020-09-02 21:19:32 +0200

    commit

--- yachtmanapi/testtest/views.py
+++ yachtmanapi/testtest/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Test
+from .serializers import TestSerializer
 
+class TestView(viewsets.ModelViewSet):
+    queryset = Test.objects.all()
+    serializer_class = TestSerializer
 # Create your views here.
